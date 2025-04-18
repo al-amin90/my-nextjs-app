d17b62d90f71aa1d17643ee602d0d646690d1404
al-amin90 <ijesun30@gmail.com>
2025-04-18 20:36:43 +0600

    commit added

--- src/app/(withCommonLayout)/layout.js
+++ src/app/(withCommonLayout)/layout.js
@@ -5,6 +5,12 @@ const CommonLayout = ({ children }) => {
   return (
     <div>
       <Navabar />
+      {/* this is not the work i want to do asdkljsldkflksadfjd kasdkl */}
+      {/* this is not the work i want to do asdkljsldkflksadfjd kasdkl */}
+      {/* this is not the work i want to do asdkljsldkflksadfjd kasdkl */}
+      {/* this is not the work i want to do asdkljsldkflksadfjd kasdkl */}
+      {/* this is not the work i want to do asdkljsldkflksadfjd kasdkl */}
+      {/* this is not the work i want to do asdkljsldkflksadfjd kasdkl */}
       <div className="min-h-screen">{children}</div>
       <div className="w-full bg-red-500 py-6 text-center"> I am footer </div>
     </div>
